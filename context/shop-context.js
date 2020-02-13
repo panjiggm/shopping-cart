41bd9c266e2f952ed0a57a04cbe47a47135ffda0
Panji Gumelar <panjiggm@gmail.com>
2020-02-13 09:27:17 +0700

    creat context provider

---
+++ context/shop-context.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default React.createContext({
+  product: [
+    { id: "p1", title: "Gaming Keyboard", price: 2000000 },
+    { id: "p2", title: "Shirohige Action Figure", price: 5000000 },
+    { id: "p3", title: "Call of Duty: Black Ops", price: 700000 },
+    { id: "p4", title: "Samsung S20 Ultra", price: 18000000 }
+  ],
+  cart: [],
+  addProductToCart: () => {},
+  removeProductFromCart: () => {}
+});
